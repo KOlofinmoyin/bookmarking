0016766e1127f43875db71f194b899951f5932d3
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-04-12 09:47:22 +0100

    included user: 'postgres' and password: '<included>' connection options to the PG object on the bookmarking lib file.

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -9,7 +9,7 @@ class Bookmarking
       #   "www.alltheweb.com",
       #   "www.google.com"
       # ]
-      connection = PG.connect(dbname: 'bookmarking_manager')
+      connection = PG.connect(dbname: 'bookmarking_manager', user: 'postgres', password: 'pa55w0rd')
       result = connection.exec("SELECT * from bookmarkings")
       result.map { |bookmark| bookmark['url']  }
       end
