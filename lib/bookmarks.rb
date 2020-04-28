7833bc281f90b3c7d0c24791011f8e9a7452e6be
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-04-28 23:12:18 +0100

    created new class methods 'update' and 'find' on the model.

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -44,5 +44,25 @@ class Bookmarking
         result = connection.exec("DELETE FROM bookmarkings WHERE id = #{id}")
       end
 
+      def update(id:, title:, url:)
+        if ENV['RACK_ENV'] == 'test'
+          connection = PG.connect(dbname: 'bookmarking_manager_test', user: 'postgres', password: 'pa55w0rd')
+        else
+          connection = PG.connect(dbname: 'bookmarking_manager', user: 'postgres', password: 'pa55w0rd')
+        end
+        result = connection.exec("UPDATE bookmarkings SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
+        Bookmarking.new(id: result[0]['id'], title: result[0]['title'],url: result[0]['url'])
+      end
+
+      def find(id:)
+        if ENV['RACK_ENV'] == 'test'
+          connection = PG.connect(dbname: 'bookmarking_manager_test', user: 'postgres', password: 'pa55w0rd')
+        else
+          connection = PG.connect(dbname: 'bookmarking_manager', user: 'postgres', password: 'pa55w0rd')
+        end
+        result = connection.exec("SELECT * FROM bookmarkings WHERE id = #{id}")
+        Bookmarking.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+      end
+
     end
 end
