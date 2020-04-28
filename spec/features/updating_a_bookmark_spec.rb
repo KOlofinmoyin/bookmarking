12c338f6bc690559f033a239eac6519b56e14753
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-04-29 00:12:12 +0100

    - added capybara feature tests for updates to bookmarks - added 'edit' button to the Index page - added a form with method 'PATCH' to the edit erb

--- spec/features/updating_a_bookmark_spec.rb
+++ spec/features/updating_a_bookmark_spec.rb
@@ -5,7 +5,7 @@ feature 'Updating a bookmark' do
     expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
 
     first('.bookmark').click_button 'Edit'
-    expect(current_path).to eq "bookmarks/#{bookmark.id}/edit"
+    expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
 
     fill_in('url', with: "http://www.snakersacademy.com")
     fill_in('title', with: "Snakers Academy")
@@ -13,6 +13,6 @@ feature 'Updating a bookmark' do
 
     expect(current_path).to eq '/bookmarks'
     expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
-    expect(page).to have_link('http://www.snakersacademy.com')
+    expect(page).to have_link('Snakers Academy', href: 'http://www.snakersacademy.com')
   end
 end
